4827ddb547442ab946bef5decc2bc404fa228fe5
thuylinh <l0903808625@gmail.com>
2020-07-11 20:57:02 +0700

    Initial Commit

---
+++ app1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by thuylinh on 7/11/20.
+//  Copyright © 2020 thuylinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
